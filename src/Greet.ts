53a424bc7a7503e8f5e796396eb9cb5c19027220
Vikash Yadav <yadav621gzp@gmail.com>
2025-01-10 21:21:48 +0530

    Chnaged

--- src/Greet.ts
+++ src/Greet.ts
@@ -1,16 +1,18 @@
 import { Field, CircuitString,Hash, SmartContract, state, State, method} from "o1js"
 
 export class Greet extends SmartContract {
-  @state(CircuitString) Greet = State<CircuitString>();
+  // @state(CircuitString) Greet = State<CircuitString>();
+
+  @state(Field) Greet = State<Field>();
 
   init() {
     super.init();
-    this.Greet.set(CircuitString.fromString('abc..xyz'));
+    this.Greet.set(CircuitString.fromString('Greeting').hash());
   }
 
   @method async update(_Greet: CircuitString) {
     // const currentState = this.Greet.get();
-    this.Greet.set(_Greet); // Removed .hash() since we want to store the CircuitString directly
+    this.Greet.set(_Greet.hash()); // Removed .hash() since we want to store the CircuitString directly
   }
 }
 
